5893df3e937a3843680da0353c7f5dd820933345
eyobScript <eyobbmulugeta@gmail.com>
2026-01-19 18:07:12 +0300

    Manage Prices in django admin with tabular inline

--- src/subscriptions/admin.py
+++ src/subscriptions/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from .models import Subscriptions,UserSubscriptions
+from .models import Subscriptions, UserSubscriptions, SubscriptionsPrice
 
-admin.site.register(Subscriptions)
+
+class SubscriptionPrice(admin.TabularInline):
+    model = SubscriptionsPrice
+    readonly_fields = ['stripe_id']
+    can_delete = False
+    extra = 0
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    inlines = [SubscriptionPrice]
+    list_display = ['name', 'active']
+
+
+admin.site.register(Subscriptions, SubscriptionAdmin)
 admin.site.register(UserSubscriptions)
