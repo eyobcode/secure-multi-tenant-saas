80231688eb14d160331165aae7abf226a903470e
eyobScript <eyobbmulugeta@gmail.com>
2026-01-20 01:08:55 +0300

    completed subscription pricing view, and template

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from subscriptions.models import SubscriptionsPrice,Subscriptions
+
+
+def subscription_price_view(request):
+    qs = SubscriptionsPrice.objects.filter(featured=True)
+    monthly_qs = qs.filter(interval=SubscriptionsPrice.IntervalChoices.MONTHLY)
+    yearly_qs = qs.filter(interval=SubscriptionsPrice.IntervalChoices.YEARLY)
+    return render(request,'subscriptions/price.html',{
+        "monthly_qs":monthly_qs,
+        "yearly_qs":yearly_qs
+    })
