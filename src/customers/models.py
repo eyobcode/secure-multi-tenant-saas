fbbb828e3fbcfbf21f608920c1e48532507292c6
eyobScript <eyobbmulugeta@gmail.com>
2026-01-21 18:01:47 +0300

    Completed: A django session Based Checkout Process

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,52 +1,39 @@
 from django.db import models
-from django.contrib.auth import settings
+from django.conf import settings
 import helpers.billing
-from allauth.account.signals import (
-    user_signed_up as allauth_user_signed_up,
-    email_confirmed as allauth_email_confirmed
-)
 
 User = settings.AUTH_USER_MODEL
 
+
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    stripe_id = models.CharField(max_length=150,null=True,blank=True)
+    stripe_id = models.CharField(max_length=120, null=True, blank=True)
     init_email = models.EmailField(blank=True, null=True)
     init_email_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
 
-    def save(self,*args, **kwargs):
-        if not self.stripe_id:
-            if self.init_email and self.init_email_confirmed:
-                email = self.user.email
-                if email is not None and email != "":
-                    self.stripe_id = helpers.billing.create_customer(
-                        raw=False,
-                        email=email,
-                        metadata={"user_id": self.user.id},
-                    )
-        super().save(*args, **kwargs)
+    def create_stripe_customer(self):
+        """
+        Explicit Stripe customer creation.
+        Safe to call multiple times.
+        """
+        if self.stripe_id:
+            return self.stripe_id
 
-def allauth_user_signed_up_handler(request,user,*args,**kwargs):
-    email = user.email
-    Customer.objects.create(
-        user=user,
-        init_email=email,
-        init_email_confirmed=False,
-    )
+        if not self.init_email_confirmed or not self.init_email:
+            return None
 
-allauth_user_signed_up.connect(allauth_user_signed_up_handler)
+        stripe_id = helpers.billing.create_customer(
+            email=self.init_email,
+            metadata={
+                "user_id": self.user.id,
+                "username": self.user.username,
+            },
+            raw=False,
+        )
 
-
-def allauth_email_confirmed_handler(request, email_address,*args,**kwargs):
-    qs = Customer.objects.filter(
-        init_email=email_address,
-        init_email_confirmed=False,
-    )
-    for obj in qs:
-        obj.init_email_confirmed=True
-        obj.save()
-
-allauth_email_confirmed.connect(allauth_email_confirmed_handler)
+        self.stripe_id = stripe_id
+        self.save(update_fields=["stripe_id"])
+        return stripe_id
