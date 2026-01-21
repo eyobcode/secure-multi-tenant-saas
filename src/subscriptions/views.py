b6d29f5c23d10e7d0d93e001136595fffc74b309
eyobScript <eyobbmulugeta@gmail.com>
2026-01-21 10:27:23 +0300

    completed interval toggle views

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-
 from subscriptions.models import SubscriptionsPrice,Subscriptions
+from django.urls import reverse
 
 
-def subscription_price_view(request):
+def subscription_price_view(request, interval="monthly"):
     qs = SubscriptionsPrice.objects.filter(featured=True)
-    monthly_qs = qs.filter(interval=SubscriptionsPrice.IntervalChoices.MONTHLY)
-    yearly_qs = qs.filter(interval=SubscriptionsPrice.IntervalChoices.YEARLY)
-    return render(request,'subscriptions/price.html',{
-        "monthly_qs":monthly_qs,
-        "yearly_qs":yearly_qs
+    inv_mo = SubscriptionsPrice.IntervalChoices.MONTHLY
+    inv_yr = SubscriptionsPrice.IntervalChoices.YEARLY
+    object_list = qs.filter(interval=inv_mo)
+    url_path_name = "pricing_interval"
+    mo_url = reverse(url_path_name, kwargs={"interval": inv_mo})
+    yr_url = reverse(url_path_name, kwargs={"interval": inv_yr})
+    active = inv_mo
+    if interval == inv_yr:
+        active = inv_yr
+        object_list = qs.filter(interval=inv_yr)
+
+    return render(request, "subscriptions/price.html", {
+        "object_list": object_list,
+        "mo_url": mo_url,
+        "yr_url": yr_url,
+        "active": active,
     })
