40bd26a30edf9d99972562e1d66e1b7b32884730
eyobScript <eyobbmulugeta@gmail.com>
2026-01-24 14:26:21 +0300

    Completed: Custom Django Management Command with Arguments

--- src/subscriptions/management/commands/sync_subscription.py
+++ src/subscriptions/management/commands/sync_subscription.py
@@ -1,26 +1,8 @@
 from django.core.management.base import BaseCommand
-from subscriptions.models import Subscriptions
-
+from typing import Any
+from subscriptions import utils as sync_subs
 
 class Command(BaseCommand):
-    help = "Sync subscription permissions to groups"
+    def handle(self, *args: Any, **options: Any):
+        sync_subs.sync_subs_group_permissions()
 
-    def handle(self, *args, **options):
-        qs = Subscriptions.objects.filter(active=True).prefetch_related(
-            "groups",
-            "permissions",
-        )
-
-        for obj in qs:
-            groups = obj.groups.all()
-            permissions = obj.permissions.all()
-
-            for group in groups:
-                group.permissions.set(permissions)
-
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Synced {obj.name} → "
-                    f"{len(groups)} groups, {len(permissions)} permissions"
-                )
-            )
